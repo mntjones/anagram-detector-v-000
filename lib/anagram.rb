d736bd52d8eb0b89f720e223b3acb0bc7aa50216
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-16 22:07:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(list)
+    match_array = []
+    compare_word = @word.split("").sort
+    compare_match = []
+    list.each do |ana|
+      compare_match = ana.split("").sort
+      if compare_match == compare_word
+        match_array << ana
+      end
+    end
+    match_array
+  end
+end
